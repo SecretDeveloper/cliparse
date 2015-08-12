2a44d7171f9681687a916b691c824233b7691847
Gary Kenneally <garykenneally@gmail.com>
2015-08-12 19:55:55 +0100

    V0.1.4

--- src/CliParse/Properties/AssemblyInfo.cs
+++ src/CliParse/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 // The following Assembly Version items are managed by the build process.
 //[assembly: AssemblyVersion("0.0.0.0")]
 //[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyVersion("0.1.3")]
-[assembly: AssemblyFileVersion("0.1.3")]
+[assembly: AssemblyVersion("0.1.4")]
+[assembly: AssemblyFileVersion("0.1.4")]
